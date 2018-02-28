d9881c9ed02f274230012e5c97bfeca0385130bc
Sam Deane <sam@elegantchaos.com>
2018-02-28 17:18:56 +0000

    Added Logger dependancy.

--- Package.swift
+++ Package.swift
@@ -7,13 +7,13 @@ let package = Package(
     name: "Builder",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "git@github.com:elegantchaos/logger.git", from: "1.0.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Builder",
-            dependencies: []),
+            dependencies: ["Logger"]),
     ]
 )
