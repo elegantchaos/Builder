9cfaac437ca772232b07729aad8ae349a57274ab
Sam Deane <sam@elegantchaos.com>
2018-02-28 17:18:28 +0000

    Added example package to use in tests.

---
+++ Example/Tests/ExampleTests/ExampleTests.swift
@@ -0,0 +1,16 @@
+import XCTest
+@testable import Example
+
+class ExampleTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Example().text, "Hello, World!")
+    }
+
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
