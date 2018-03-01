c948a7dcffbcdc057007e380d3935bf50d3724a3
Sam Deane <sam@elegantchaos.com>
2018-03-01 15:18:09 +0000

    added headers

--- Example/Sources/Tool/main.swift
+++ Example/Sources/Tool/main.swift
@@ -1,3 +1,9 @@
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// Created by Sam Deane, 28/02/2018.
+// All code (c) 2018 - present day, Elegant Chaos Limited.
+// For licensing terms, see http://elegantchaos.com/license/liberal/.
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
 let phase = CommandLine.arguments[1]
 
 print("I'm an example build tool.")
--- Example/Tests/ExampleTests/ExampleTests.swift
+++ Example/Tests/ExampleTests/ExampleTests.swift
@@ -1,3 +1,9 @@
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// Created by Sam Deane, 28/02/2018.
+// All code (c) 2018 - present day, Elegant Chaos Limited.
+// For licensing terms, see http://elegantchaos.com/license/liberal/.
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
 import XCTest
 @testable import Example
 
